c0871a9ec73b7f5a26de844106189f52c936a246
danielfasure <danielfasure@gmail.com>
2026-09-04 16:59:33 +0100

    extra small change

--- frontend/index.js
+++ frontend/index.js
@@ -7,8 +7,8 @@ let register_portal = document.querySelector(".register_div");
 let button_change_register = document.querySelector(".change_register_button");
 button_change_register.addEventListener("click",()=>{
 
-    register_portal.classList.remove("hidden")
-      login_portal.classList.add("hidden")
+    register_portal.classList.toggle("hidden")
+      login_portal.classList.toggle("hidden")
 });
 
 
@@ -16,6 +16,6 @@ let button_change_login = document.querySelector(".change_login_button");
 button_change_login.addEventListener("click",()=>{
     
 
-  register_portal.classList.add("hidden")
- login_portal.classList.remove("hidden")
+  register_portal.classList.toggle("hidden")
+ login_portal.classList.toggle("hidden")
 });
