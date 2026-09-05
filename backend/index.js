d04d187ee780106df49def5b699255cb953d769e
danielfasure <danielfasure@gmail.com>
2026-09-05 21:30:52 +0100

    extra small change for css again

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "node:url";
+const __dirname =dirname(fileURLToPath(import.meta.url));
+const __parentDirectory = path.join(__dirname, "..");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(__parentDirectory+"/frontend/index.html");
+  
+});
+app.get("/")
+
+app.listen(port,()=>{
+    console.log(`services running on host port ${port}`);
+
+});
+
+
+
+
