03127f69ddb87a77edde82a4223bf2232af969f7
danielfasure <danielfasure@gmail.com>
2026-09-12 14:27:09 +0100

    fixing structure

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,72 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true}));
+
 
 
 app.use(express.static("frontend"));
 
 
+const client = new pg.Client({
+    user: "daniel",
+    host: "localhost",
+    database: "job_tracker_db",
+    password: "Ayomikun12!",
+    port: 5432
+});
+
+client.connect();
 
 
-app.post("/submit",(req,res)=>{
-  const  username = req.body.username;
- const   password = req.body.password;
- res.render("application_tracker.ejs",{
-    daytype:username,
-    day_password:password
+
+
+
+
+
+let jobs = []
+let job = {}
+function query(req, res) {
+
+    client.query("SELECT * FROM jobinfo", (err, result) => {
+
+        if (err) {
+            console.error("Error doing this query", err.stack);
+            return res.status(500).send("Database error");
+            
+        }
+
+        const jobs = result.rows;
+
+        res.render("tracker.ejs", { job: jobs });
+
+        
     });
+}
+
+
+
+
+
+
+
+
+app.post("/portal",(req,res)=>{
+  const {data} = req.body;
+  
+  console.log()
+ res.render("application_tracker.ejs",)
+    
 })
+
+app.get("/portal",(req,res)=>{
+ res.render("application_tracker.ejs");
+
+});
 
 app.get("/home",(req,res)=>{
  res.render("index.ejs");
@@ -34,7 +80,10 @@ app.get("/home",(req,res)=>{
   
 }); */
 
-//app.get("/")
+app.get("/tracker",  (req,res)=>{
+    query(req,res)
+    
+})
 
 app.listen(port,()=>{
     console.log(`services running on host port ${port}`);
