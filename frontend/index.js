53956e01dbbc789bb647ef5f17c61d8bf8c0cc99
danielfasure <danielfasure@gmail.com>
2026-09-25 16:11:43 +0100

    frontend changes

--- frontend/index.js
+++ frontend/index.js
@@ -42,3 +42,97 @@ if (viewApplicationButton && viewApplication) {
         
     });
 }
+
+
+const applicationHeaders = document.querySelectorAll(".application-header");
+
+applicationHeaders.forEach(header => {
+
+    header.addEventListener("click", () => {
+
+        const card = header.closest(".application-card");
+
+        card.classList.toggle("active");
+
+    });
+
+});
+
+
+// SETTINGS
+const editPasswordButton = document.querySelector("#edit_password_button");
+const editEmailButton = document.querySelector("#edit_email_button");
+
+const passwordDisplay = document.querySelector("#password_display");
+const passwordInput = document.querySelector("#password_input");
+
+const emailDisplay = document.querySelector("#email_display");
+const emailInput = document.querySelector("#email_input");
+
+
+if (editPasswordButton) {
+
+    editPasswordButton.addEventListener("click", () => {
+
+        const editing = !passwordInput.classList.contains("hidden");
+
+        if (editing) {
+
+            // CANCEL
+            passwordInput.value = "";
+
+            passwordInput.classList.add("hidden");
+            passwordDisplay.classList.remove("hidden");
+
+            editPasswordButton.innerHTML =
+                `<i class="bi bi-pencil-square"></i> Edit`;
+
+        } else {
+
+            // EDIT
+            passwordInput.classList.remove("hidden");
+            passwordDisplay.classList.add("hidden");
+
+            editPasswordButton.innerHTML =
+                `<i class="bi bi-x-circle"></i> Cancel`;
+
+            passwordInput.focus();
+        }
+
+    });
+
+}
+
+
+if (editEmailButton) {
+
+    editEmailButton.addEventListener("click", () => {
+
+        const editing = !emailInput.classList.contains("hidden");
+
+        if (editing) {
+
+            // CANCEL
+            emailInput.value = emailDisplay.textContent.trim();
+
+            emailInput.classList.add("hidden");
+            emailDisplay.classList.remove("hidden");
+
+            editEmailButton.innerHTML =
+                `<i class="bi bi-pencil-square"></i> Edit`;
+
+        } else {
+
+            // EDIT
+            emailInput.classList.remove("hidden");
+            emailDisplay.classList.add("hidden");
+
+            editEmailButton.innerHTML =
+                `<i class="bi bi-x-circle"></i> Cancel`;
+
+            emailInput.focus();
+        }
+
+    });
+
+}
